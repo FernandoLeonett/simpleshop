5f933dd32b10978d9a5bf700fc6622a0fc9be1d1
Fernando <fernandoleonett@gmail.com>
2022-07-12 15:56:21 -0300

    user data

--- utils/userdata.ts
+++ utils/userdata.ts
@@ -1,1 +1,6 @@
 export const whatshapNumber = "59899297242";
+// export const urlCliente =
+// "https://docs.google.com/spreadsheets/d/e/2PACX-1vSk6SRbveAHR5DVL_DoFCMXNNMV37BD-J-GzY9i8u246Gwiv5MstNkRSRi2VJNln3W5PaIduadgiRg2/pub?output=csv";
+
+export const urlCliente =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vR759F7fgBqKXwjYoEul67aWjUkmUoS4dTHQwo14pIa6qtPlPqu7d0jYGLs2ef8VNqfGz8k37NgEapK/pub?output=csv";
