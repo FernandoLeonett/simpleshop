850c5a19ef95fb4cd0555279e2baf977cd118572
Fernando <fernandoleonett@gmail.com>
2022-07-12 11:14:06 -0300

    ready images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {GetStaticProps} from "next";
-import {Button, Flex, Grid, Link, Stack, Text} from "@chakra-ui/react";
+import {Button, Flex, Image, Grid, Link, Stack, Text, Center} from "@chakra-ui/react";
+import {motion, AnimatePresence, AnimateSharedLayout} from "framer-motion";
 
 import {Product} from "../product/types";
 import api from "../product/api";
@@ -19,6 +20,7 @@ function parseCurrency(value: number): string {
 
 const IndexRoute: React.FC<Props> = ({products}) => {
   const [cart, setCart] = React.useState<Product[]>([]);
+  const [selectedImage, setSelectedImage] = React.useState<string>(null);
   const text = React.useMemo(
     () =>
       cart
@@ -34,47 +36,80 @@ const IndexRoute: React.FC<Props> = ({products}) => {
   );
 
   return (
-    <Stack spacing={6}>
-      <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
-        {products.map((product) => (
-          <Stack
-            key={product.id}
-            backgroundColor="gray.100"
-            borderRadius="md"
-            padding={4}
-            spacing={3}
-          >
-            <Stack spacing={1}>
-              <Text>{product.title}</Text>
-              <Text color="green.500" fontSize="sm" fontWeight="500">
-                {parseCurrency(product.price)}
-              </Text>
-            </Stack>
-            <Button
-              colorScheme="primary"
-              size="sm"
-              variant="outline"
-              onClick={() => setCart((cart) => cart.concat(product))}
+    <AnimateSharedLayout type="crossfade">
+      <Stack spacing={6}>
+        <Grid gridGap={6} templateColumns="repeat(auto-fill, minmax(240px, 1fr))">
+          {products.map((product) => (
+            <Stack
+              key={product.id}
+              backgroundColor="gray.100"
+              borderRadius="md"
+              padding={4}
+              spacing={3}
             >
-              Agregar
+              <Stack spacing={1}>
+                <Image
+                  alt={product.title}
+                  as={motion.img}
+                  borderTopRadius="md"
+                  cursor={"pointer"}
+                  layoutId={product.image}
+                  maxHeight={128}
+                  objectFit={"cover"}
+                  src={product.image}
+                  onClick={() => setSelectedImage(product.image)}
+                />
+                <Text>{product.title}</Text>
+                <Text color="green.500" fontSize="sm" fontWeight="500">
+                  {parseCurrency(product.price)}
+                </Text>
+              </Stack>
+              <Button
+                colorScheme="primary"
+                size="sm"
+                variant="outline"
+                onClick={() => setCart((cart) => cart.concat(product))}
+              >
+                Agregar
+              </Button>
+            </Stack>
+          ))}
+        </Grid>
+        {Boolean(cart.length) && (
+          <Flex alignItems="center" bottom={4} justifyContent="center" position="sticky">
+            <Button
+              isExternal
+              as={Link}
+              colorScheme="whatsapp"
+              href={`https://wa.me/${whatshapNumber}?text=${encodeURIComponent(text)}`}
+              width="fit-content"
+            >
+              Completar pedido ({cart.length} productos)
             </Button>
-          </Stack>
-        ))}
-      </Grid>
-      {Boolean(cart.length) && (
-        <Flex alignItems="center" bottom={4} justifyContent="center" position="sticky">
-          <Button
-            isExternal
-            as={Link}
-            colorScheme="whatsapp"
-            href={`https://wa.me/${whatshapNumber}?text=${encodeURIComponent(text)}`}
-            width="fit-content"
+          </Flex>
+        )}
+      </Stack>
+      <AnimatePresence>
+        {selectedImage && (
+          <Flex
+            key="brackdrop"
+            alignItems="center"
+            as={motion.div}
+            backgroundColor="rgba(0,0,0,0,5)"
+            height="100%"
+            justifyContent="center"
+            layoutId={selectedImage}
+            left={0}
+            position="fixed"
+            top={0}
+            width="100%"
+            onClick={() => setSelectedImage(null)}
           >
-            Completar pedido ({cart.length} productos)
-          </Button>
-        </Flex>
-      )}
-    </Stack>
+            <Image key="image" src={selectedImage} />
+          </Flex>
+        )}
+      </AnimatePresence>
+    </AnimateSharedLayout>
   );
 };
 
