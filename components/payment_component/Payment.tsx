990610184d923cd8ce9dbfc11c7abf21d1ad5238
Fernando <fernandoleonett@gmail.com>
2022-07-22 00:14:13 -0300

    repeating

--- components/payment_component/Payment.tsx
+++ components/payment_component/Payment.tsx
@@ -2,18 +2,18 @@ import { HStack } from "@chakra-ui/react";
 import MercadoPago from "./MercadoPago";
 import Whatsaap from "./Whatsaap";
 
-interface prop {
+interface PaymentProp {
   wap?: boolean;
   mp?: boolean;
 }
 
-const Payment = ({ wap = true, mp=true }: prop) => {
+const Payment = ({ wap = true, mp = true }: PaymentProp) => {
   return (
     <HStack>
       {wap && <Whatsaap />}
       {mp && <MercadoPago />}
     </HStack>
-  )
+  );
 };
 
 export default Payment;
